6cd6f9496431d76aff13407c83ccfcabe042b5f1
dudamarqs <eduardamarquesnoleto@gmail.com>
2024-06-29 00:40:23 -0300

    Add program to calculate arithmetic mean of student grades

---
+++ atividade7.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia duas notas de um aluno, calcule e moste sua média aritmética.
+
+n1 = float(input('Digite a primeira nota do aluno: '))
+n2 = float(input('Digite a segunda nota do aluno: '))
+media_a = (n1 + n2) / 2
+print('A média aritmética do aluno é {}'.format(media_a))

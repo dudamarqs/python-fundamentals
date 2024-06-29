a27f85f4e395775a4c61a3044e146fddb4c2acf6
dudamarqs <eduardamarquesnoleto@gmail.com>
2024-06-29 12:30:17 -0300

    Add program for Celsius to Fahrenheit temperature conversion

---
+++ atividade13.py
@@ -0,0 +1,3 @@
+salary = float(input('Informe o salário do funcionário: \nR$'))
+new_salary = salary * 1.15
+print('\nNovo salário do funcionário (com 15% de aumento): R${:.2f}.'.format(new_salary))

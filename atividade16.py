ce958af7670a595e1538dd0e8d41ebe33681e34b
dudamarqs <eduardamarquesnoleto@gmail.com>
2024-06-30 16:11:30 -0300

    Program to display integer portion of a real number using the math.trunc() function

---
+++ atividade16.py
@@ -0,0 +1,6 @@
+# import math (This imports all features)
+# from math import ... (This only imports the requested function)
+from math import trunc
+
+num = float(input('Digite um número real: \n'))
+print('\nPorção inteira do número: {}.'.format(trunc(num)))

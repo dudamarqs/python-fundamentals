a0f3bc88076bf76e0bcca5f0e426b443028106b1
dudamarqs <eduardamarquesnoleto@gmail.com>
2024-07-02 20:38:42 -0300

    Code to check if three segments can form a triangle

---
+++ atividade35.py
@@ -0,0 +1,9 @@
+print('Analisando triângulos:\n')
+s1 = float(input('Primeiro segmento: '))
+s2 = float(input('Segundo segmento: '))
+s3 = float(input('Terceiro segmento: '))
+
+if (s1 < s2 + s3) and (s2 < s2 + s3) and (s3 < s2 + s1):
+    print('Os segmentos podem formar um triângulo.')
+else:
+    print('Os segmentos não podem formar um triângulo.')

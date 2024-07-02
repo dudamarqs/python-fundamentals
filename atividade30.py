a1885a5aaf4861c31321ad9f41d7ea4a9cc84991
dudamarqs <eduardamarquesnoleto@gmail.com>
2024-07-01 21:47:34 -0300

    Program to check is the number is odd or even

---
+++ atividade30.py
@@ -0,0 +1,6 @@
+num = int(input('Digite um número para verificar se ele é ímpar ou par: '))
+result = num % 2
+if result == 0:
+    print('O número é par!\n')
+else:
+    print('O número é ímpar!\n')
